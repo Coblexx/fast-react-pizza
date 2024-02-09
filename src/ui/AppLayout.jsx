a97379e6119aa8867f3270164589d20a2971d349
radek <radek.glogowski3@gmail.com>
2024-02-09 17:48:03 +0100

    chore: error handling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,18 @@
 import Header from "./Header";
 import CartOverview from "../features/Cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
+
       <main>
         {/* Tool used to render a nested route */}
         <Outlet />
