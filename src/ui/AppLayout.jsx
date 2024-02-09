00ce9c5801c29b19243de629f18c3271fb56198e
radek <radek.glogowski3@gmail.com>
2024-02-09 17:18:52 +0100

    chore: create app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,19 @@
+import Header from "./Header";
+import CartOverview from "../features/Cart/CartOverview";
+import { Outlet } from "react-router-dom";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Tool used to render a nested route */}
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
