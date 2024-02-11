0876294cf8a58f83b89622b635956a9c75453ed2
radek <radek.glogowski3@gmail.com>
2024-02-11 19:33:43 +0100

    chore: further styling, create username component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/Order/SearchOrder";
+import Username from "../features/User/Username";
 
 function Header() {
   return (
     <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+      <Link to="/" className="uppercase tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 }
