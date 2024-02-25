10cf56f665f32a87902ef417332658b78beb3ca3
radek <radek.glogowski3@gmail.com>
2024-02-25 17:33:48 +0100

    chore: add clear cart

--- src/features/Cart/Cart.jsx
+++ src/features/Cart/Cart.jsx
@@ -1,8 +1,8 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-import { getCart } from "./cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
 import { getUsername } from "../User/userSlice";
 
 // const fakeCart = [
@@ -34,6 +34,8 @@ function Cart() {
   // const cart = fakeCart;
   const cart = useSelector(getCart);
 
+  const dispatch = useDispatch();
+
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -50,7 +52,9 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
