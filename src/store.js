a5c9bd850eeecc90d59988817336a769b2167f1f
radek <radek.glogowski3@gmail.com>
2024-02-25 17:20:27 +0100

    chore: add item to cart

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/User/userSlice";
+import cartReducer from "./features/Cart/cartSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
