4f0783912dbf59fef8b485bcebdac7ce4683845f
radek <radek.glogowski3@gmail.com>
2024-03-03 13:19:32 +0100

    chore: update item quantity in menu and cart

--- src/features/Menu/MenuItem.jsx
+++ src/features/Menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from "../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from "../Cart/cartSlice";
 import DeleteItem from "../Cart/DeleteItem";
+import UpdateItemQuantity from "../Cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -43,7 +44,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
